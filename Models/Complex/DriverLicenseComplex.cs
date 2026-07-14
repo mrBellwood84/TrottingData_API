be26ab4ff8b24a05300c78d3dc600fb68130b2e4
mrb <kristian.w.finstad@gmail.com>
2026-07-14 10:41:04 +0200

    Model update

--- Models/Complex/DriverLicenseComplex.cs
+++ Models/Complex/DriverLicenseComplex.cs
@@ -4,7 +4,7 @@ namespace Models.Complex;
 
 public class DriverLicenseComplex : IDbItem
 {
+    public string Id { get; init; }
     public string Code { get; init; }
     public string Description { get; init; }
-    public string Id { get; init; }
 }
